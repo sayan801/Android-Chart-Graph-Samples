4529dee78aad48dbcec0e249219b9808c057fc56
technicise <amiyo.biswas@mycuratio.com>
2014-11-27 19:29:36 +0530

    Removed Abundant Code & Improve Functionality

--- CustomLineChart/app/src/main/java/com/example/technicise/customlinechart/ContainerScrollType.java
+++
@@ -1,9 +0,0 @@
-package com.example.technicise.customlinechart;
-
-/**
- * Enum used to inform chart in what type of container it exists.
- * 
- */
-public enum ContainerScrollType {
-	HORIZONTAL, VERTICAL
-}
--- CustomLineChart/app/src/main/java/com/example/technicise/customlinechart/ZoomType.java
+++
@@ -1,6 +0,0 @@
-package com.example.technicise.customlinechart;
-
-public enum ZoomType {
-
-	HORIZONTAL, VERTICAL, HORIZONTAL_AND_VERTICAL;
-}
